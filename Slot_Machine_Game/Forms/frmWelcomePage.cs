750978ce2454a922e529eccd35003588d7287aae
Andrew Larkins <alarkins93@yahoo.com>
2020-03-20 15:35:11 -0400

    First Commit

---
+++ Slot_Machine_Game/Forms/frmWelcomePage.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Andrew Larkins
+ * Eric Friedman
+ * CIS-3309
+ * Slot Machine Game
+ * 03/20/20
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Welcome form to get user's name and starting money balance.
+
+namespace Slot_Machine_Game
+{
+    public partial class frmWelcomePage : Form
+    {
+        public frmWelcomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
