e174b1ba5b357af4898dcce305b36ebf1cba5b86
Andrew Larkins <alarkins93@yahoo.com>
2020-03-25 00:52:30 -0400

    Commit 3

--- Slot_Machine_Game/Classes/PlayerClass.cs
+++ Slot_Machine_Game/Classes/PlayerClass.cs
@@ -12,19 +12,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Player class used to store scores
-// of the player.
+// Player class used to store name
+// and money balance of the player.
 
 namespace Slot_Machine_Game.Classes
 {
     class PlayerClass
     {
+        // class attributes.
+        private string name;
+        private int balance = 0;
+        private int finalScore = 0;
+        
 
-        // Constructor.
-        public PlayerClass()
+        // Constructor sets name and balance.
+        public PlayerClass(string name, int balance)
         {
-
+            this.name = name;
+            this.balance = Math.Abs(balance);
         }
+
+        // Getter methods.
+        public string getName()
+        {
+            return name;
+        }
+        public int getBalance()
+        {
+            return balance;
+        }
+        public int getScore()
+        {
+            return finalScore;
+        }
+        // Setter method.
+        public void setScore(int finalScore)
+        {
+            this.finalScore = finalScore;
+        }
+
+        // Return score.
+        public int calculateScore()
+        {
+            return finalScore = finalScore - balance;
+        }
+
+        // Reset to be used after game is over.
+        public void reset()
+        {
+            name = "";
+            balance = 0;
+            finalScore = 0;
+            
+        }
+
+        public string playerToString()
+        {
+            string strFinalScore = name + ": " + finalScore;
+            return strFinalScore;
+        }
+
 
     }
 }
